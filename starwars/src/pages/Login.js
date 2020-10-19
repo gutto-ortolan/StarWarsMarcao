50dd72fe2d490bca349a14688e2c68d08090b009
Augusto Kruger Ortolan <augusto.ortolan@parceria.local>
2020-10-19 13:58:55 -0300

    modificações

--- starwars/src/pages/Login.js
+++ starwars/src/pages/Login.js
@@ -1,35 +1,42 @@
-import React from 'react'
-import {Button, Form, Card} from 'react-bootstrap'
+import React from "react";
+import { Button, Form, Card } from "react-bootstrap";
 
 export default function Login() {
-
-    return (
+  return (
     <>
-        <section id="three" className="login-page">
-            <div className="inner login-center">
-                <Card style={{width: '50%'}}className="box">
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email"/>
-                            <Form.Text className="text-muted">
-                                Não passe as suas credenciais para mais ninguém.
-                            </Form.Text>
-                        </Form.Group>
+      <section id="three" className="wrapper special login-page">
+        <div className="inner login-center">
+          <Card style={{ width: "50%" }} className="box">
+            <Form>
+              <Form.Group
+                controlId="formBasicEmail"
+                className="alinharEsquerda"
+              >
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" />
+                <Form.Text className="text-muted alinharEsquerda">
+                  Não passe as suas credenciais para mais ninguém.
+                </Form.Text>
+              </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control type="password" placeholder="Senha"/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Entrar
-                        </Button>
-                    </Form>
-                </Card>
-                </div>
-            </section>
-        </>
-        )
-        
-        
-        }
+              <Form.Group
+                controlId="formBasicPassword"
+                className="alinharEsquerda"
+              >
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="Senha" />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+                className="alinharEsquerda"
+              >
+                Entrar
+              </Button>
+            </Form>
+          </Card>
+        </div>
+      </section>
+    </>
+  );
+}
